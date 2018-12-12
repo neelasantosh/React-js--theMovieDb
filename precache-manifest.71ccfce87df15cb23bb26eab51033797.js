fb49555c34ad3986f390d9cb0f19149212dba32d
neela.santosh2405@gmail.com <neela.santosh2405@gmail.com>
2018-12-12 23:39:48 +0530

    Updates

---
+++ precache-manifest.71ccfce87df15cb23bb26eab51033797.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "27b65cb40d26f78354a4ac5abf87b2be",
+    "url": "/themoviedb/static/media/tmdb.27b65cb4.svg"
+  },
+  {
+    "revision": "c25c445e82ce586c70c1",
+    "url": "/themoviedb/static/js/runtime~main.c25c445e.js"
+  },
+  {
+    "revision": "20ed0377b2471027fa63",
+    "url": "/themoviedb/static/js/main.20ed0377.chunk.js"
+  },
+  {
+    "revision": "5b265051865492349dc6",
+    "url": "/themoviedb/static/js/1.5b265051.chunk.js"
+  },
+  {
+    "revision": "20ed0377b2471027fa63",
+    "url": "/themoviedb/static/css/main.cfd76ec7.chunk.css"
+  },
+  {
+    "revision": "23e4500a5b185ac1c30fabf3fae855ac",
+    "url": "/themoviedb/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/neelasantosh/themoviedb/precache-manifest.3b92293f033a11ea29b8772e4b47ffda.js"
+  "/themoviedb/precache-manifest.71ccfce87df15cb23bb26eab51033797.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/neelasantosh/themoviedb/index.html", {
+workbox.routing.registerNavigationRoute("/themoviedb/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
